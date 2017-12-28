3fe96204f255764ed0d22d1c8e8f327f4aabb571
João <joao@nico>
2017-12-28 21:52:32 -0200

    Add jsDoc

--- source/Router.class.js
+++ source/Router.class.js
@@ -1,6 +1,13 @@
+/**
+* Class represents the initial application router
+* this instance will make the magic happens
+* @class
+* @author  João Ribeiro <jlribeiroaugusto@gmail.com>
+*/
 
 class Router {
 
 }
 
+/** @module Router */
 export default Router
